7a0e3747539aad5c9c9476bcd07ca91862f8d15a
Erik-Alejo <133018247+Erik-Alejo@users.noreply.github.com>
2025-06-24 10:31:26 -0400

    Código Inicial ga

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+using UnityEngine.SceneManagement; 
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject jugador;
+    [SerializeField] Transform puntoDeInicio;
+
+    [SerializeField] float tiempoInicial;
+    [SerializeField] TextMeshProUGUI textoContador;
+
+    [SerializeField] GameObject pantallaGanaste;
+    [SerializeField] GameObject pantallaPerdiste;
+
+    private float tiempoRestante;
+    private bool juegoActivo = true;
+
+    void Start()
+    {
+        tiempoRestante = tiempoInicial;
+        juegoActivo = true;
+        pantallaGanaste.SetActive(false);
+        pantallaPerdiste.SetActive(false);
+
+        CharacterController cc = jugador.GetComponent<CharacterController>();
+        if (cc != null)
+        {
+            cc.enabled = false;
+            jugador.transform.position = puntoDeInicio.position;
+            cc.enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        if (!juegoActivo)
+        {
+            return;
+        }
+
+        tiempoRestante -= Time.deltaTime;
+
+        textoContador.text = "Tiempo: " + tiempoRestante.ToString("F0"); //numero sin decimales
+
+        if (tiempoRestante <= 0)
+        {
+            tiempoRestante = 0; // Para que no muestre números negativos
+            textoContador.text = "Tiempo: 0";
+            PerderJuego();
+        }
+    }
+
+    public void RespawnJugador()
+    {
+        //juego terminado no hacer nada
+        if (!juegoActivo) return;
+
+        Debug.Log("Jugador ha caído. Reubicando...");
+
+        CharacterController cc = jugador.GetComponent<CharacterController>();
+        if (cc != null)
+        {
+            cc.enabled = false; // desabilita el controlador
+            jugador.transform.position = puntoDeInicio.position; // Movemos el transform
+            jugador.transform.rotation = puntoDeInicio.rotation; // reseteamos la rotación
+            cc.enabled = true; // volvemos a habilitar el contrl
+        }
+        else
+        {
+            jugador.transform.position = puntoDeInicio.position;
+        }
+    }
+
+    public void GanarJuego()
+    {
+        if (juegoActivo)
+        {
+            Debug.Log("¡GANASTE!");
+            juegoActivo = false;
+            pantallaGanaste.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void PerderJuego()
+    {
+        if (juegoActivo)
+        {
+            Debug.Log("¡PERDISTE!");
+            juegoActivo = false;
+            pantallaPerdiste.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
